90bfdd308e8d63d78aa8065b7b51d5a8a40dc857
annayamamoto <56916503+annayamamoto@users.noreply.github.com>
2019-11-04 19:43:35 +0900

    messagecreate

--- db/migrate/20191104031511_create_group_users.rb
+++ db/migrate/20191104031511_create_group_users.rb
@@ -4,6 +4,6 @@ class CreateGroupUsers < ActiveRecord::Migration[5.0]
       t.references :group, foreign_key: true
       t.references :user, foreign_key: true
       t.timestamps  
-      end
+    end
   end
 end
