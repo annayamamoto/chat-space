0755265dbab8ba5edf5b7cb381373c9fe9221c96
annayamamoto <56916503+annayamamoto@users.noreply.github.com>
2019-11-06 11:28:10 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-<<<<<<< HEAD
-=======
-        
->>>>>>> 2b536f94ec4c1dab86783b953537efc9d4237242
   has_many :messages
   has_many :group_users
+  has_many :messages
   has_many :groups, through: :group_users
 end
