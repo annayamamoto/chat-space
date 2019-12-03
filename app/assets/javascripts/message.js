a348e85ada772e625c04cbf8365e3e592f39bc9f
annayamamoto <56916503+annayamamoto@users.noreply.github.com>
2019-12-03 18:00:51 +0900

    修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,5 @@
 $(function() {
   function buildHTML(message) {
-    console.log(message.image)
       var content = message.content ? `<p class="lower-message__content">${message.content}</p>`:``;
       var image = message.image ? `<img src="${message.image}" class="lower-message__image">`:``;
       var html = `<div class="message" data-message-id="${message.id}">
@@ -32,16 +31,11 @@ $(function() {
         contentType: false,
       })
       .done(function(data){
-        if ( data.content != undefined ){
         var html = buildHTML(data);
         $('.messages').append(html);
         $('#new_message')[0].reset();
         $(".form__submit").prop("disabled", false);
         $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
-        }
-        else{
-          alert("メッセージを入力してください")
-        }
       })
       .fail(function(){
         alert('メッセージの送信に失敗しました');
@@ -50,15 +44,15 @@ $(function() {
 
     var interval = setInterval(function() {
       if (window.location.href.match(/\/groups\/\d+\/messages/)) {
-        var href = 'api/messages'
         var last_message_id = $(".message:last").data("message-id");
+        console.log()
         $.ajax({
-          url: href,
+          url: "api/messages",
           type: "GET",
           data: {id: last_message_id},
           dataType: "json",
         })
-        .done(function(data) {
+        .done(function(messages) {
           var insertHTML = '';
           messages.forEach(function (message) {
             insertHTML = buildHTML(message)
